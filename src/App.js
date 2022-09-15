001bf956392edcd6237f4a6f7593e9cf61aa9a22
Luis <lumalof@gmail.com>
2022-09-15 17:50:02 -0500

    Preparado para iniciar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Selector from './components/Selector';
+import Slide from './components/Slider';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <hr />
+     <Slide />
+     <hr />
+     <Selector />
+     <hr />
+     <Footer />
     </div>
   );
 }
---
+++ src/components/Slider/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Slide = () => {
+  return (
+    <div>
+        <h1>Slider</h1>
+    </div>
+  )
+}
+
+export default Slide
